435d8d3f707e8d49a6903fcd0231a3289bbae292
Brittany McClellan <brittanyhines36@yahoo.com>
2022-03-18 13:34:24 -0500

    var exercise done

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace VariablesEX
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string myName = "Brittany";
+            int myAge = 25;
+            char middleInit = 'J';
+            bool isMarried = true;
+            double myWeight = 165.21;
+
+            Console.WriteLine($" My name is { myName }, I am {myAge} years old.");
+
+
+
+        }
+    }
+}
